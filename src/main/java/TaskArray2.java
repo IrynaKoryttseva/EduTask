2ee14ec90c1e8f52eae9b44f121f7cdf76d044a2
IrynaKoryttseva <Iryna_Koryttseva@epam.com>
2015-12-23 12:52:36 +0200

    done task 2

---
+++ src/main/java/TaskArray2.java
@@ -0,0 +1,19 @@
+/*
+Найти среднее арифметическое всех элементов массива
+ */
+public class TaskArray2 {
+    int[] number = {20, -15, 28, 95, 10, 10};
+    double average;
+    int sum = 0;
+    int count = 0;
+
+    public void numberAverageArray() {
+        System.out.println("task 2");
+        for (int i = 0; i < number.length; i++) {
+            sum = sum + number[i];
+            count = count + 1;
+        }
+        average = (double) sum / count;
+        System.out.println(average);
+    }
+}
