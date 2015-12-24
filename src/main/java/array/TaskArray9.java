bb0f8f9b820a465a53b6703f30da84ef3c59c721
IrynaKoryttseva <Iryna_Koryttseva@epam.com>
2015-12-24 12:44:21 +0200

    done task 9

---
+++ src/main/java/array/TaskArray9.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+
+/*
+ *  Создайте массив из 20-ти первых чисел Фибоначчи и выведите его на экран. Напоминаем, что первый и второй члены
+ *  последовательности равны единицам, а каждый следующий — сумме двух предыдущих.
+ */
+public class TaskArray9 {
+
+    int[] num= new int[20];
+
+
+    public void createArrayFibonachi(){
+        num[0]=0;
+        num[1]=1;
+    for(int i=0; i<num.length; i++){
+        try{
+         num[i+2]=num[i]+num[i+1];
+        }catch(ArrayIndexOutOfBoundsException e){ }
+    }
+        System.out.println(Arrays.toString(num));
+    }
+}
